08c1b6bf7d60e8bd60540ee2d9a6dd7c210fb9d4
Dhieu <112936899+Dhie-boop@users.noreply.github.com>
2024-04-17 11:12:31 +0000

    Few changes to the projects site

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "David Dhieu",
+  title: "Back-End Developer",
+  email: "dhieumajok211@gmail.com",
+  gitHub: "Dhie-boop",
+  instagram: "",
+  linkedIn: "dhieu-david",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Mapeei_D_D",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
